ba0c908d53051ce61ca1d417b88e3f23553c22d3
Avaiyajay <avaiyajaykumar5555@gmail.com>
2022-05-03 22:21:59 +0530

    added .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv")
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
-const blogRoute = require("./routes/blog")
-const logoRoute = require("./routes/logo")
-
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const blogRoute = require("./routes/blog");
+const logoRoute = require("./routes/logo");
+const path = require("path");
 
 dotenv.config();
 
-
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("DB is connected")
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
@@ -27,8 +26,10 @@ app.use("/api/users", userRoute);
 app.use("/api/blogs", blogRoute);
 app.use("/api/logos", logoRoute);
 
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log("backend server is running")
-})
+  console.log("backend server is running");
+});
